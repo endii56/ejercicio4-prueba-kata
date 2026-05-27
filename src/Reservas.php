b329a6a3dd6279cda424eca50420f2a1b0be92d5
Endika_L <endik200302@gmail.com>
2026-05-27 21:59:59 +0200

    [VERDE] - Test crear reservas al principio esta vacio

--- src/Reservas.php
+++ src/Reservas.php
@@ -4,7 +4,19 @@ namespace Deg540\ReservasRestaurante;
 
 class Reservas
 {
+    private array $reservas;
+
+    public function __construct(){
+        $this->reservas = [];
+    }
     public function holaMundo():string{
         return "Hola mundo";
+    }
+
+    public function ejecutar(string $accion):string{
+        if(empty($this->reservas)){
+            return "";
+        }
+        return "no esta vacio";
     }
 }
