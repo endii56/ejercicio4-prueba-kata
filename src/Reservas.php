99a1bb0de373c4ce08880e21751c78bc76fbd034
Endika_L <endik200302@gmail.com>
2026-05-27 22:12:02 +0200

    [REFACTOR] - Separar responsabilidades de la funcion principal, crear constantes.

--- src/Reservas.php
+++ src/Reservas.php
@@ -6,25 +6,33 @@ class Reservas
 {
     private array $reservas;
 
+    const string COMANDO_RESERVAR = "reservar";
+
     public function __construct(){
         $this->reservas = [];
     }
 
     public function ejecutar(string $accion):string{
+        [$comando, $nombre, $numero] = $this->obtenerParametros($accion);
+        if($comando === self::COMANDO_RESERVAR){
+            $this->reservar($nombre, $numero);
+        }
+        return $this->listaReservas();
+    }
+
+    private function obtenerParametros(string $accion):array{
         $separado = explode(" ", $accion);
-        $comando = $separado[0];
-        $nombre = $separado[1];
-        $numero = $separado[2];
-        if($comando === "reservar"){
-            $this->reservas[$nombre] = $numero;
-        }
-        if(empty($this->reservas)){
-            return "";
-        }
+        return [$separado[0], $separado[1], $separado[2]];
+    }
+
+    private function listaReservas():string{
         $devolucionReservas = [];
         foreach($this->reservas as $nombre => $numero){
             $devolucionReservas[] = "$nombre x$numero";
         }
         return implode(", ", $devolucionReservas);
+    }
+    private function reservar(string $nombre, int $numero):void{
+        $this->reservas[$nombre] = $numero;
     }
 }
